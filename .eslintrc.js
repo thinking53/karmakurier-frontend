58ca4de8e2156fb98e7adf7a4ca7cd3f8fa7b3fe
Sven Vowe <svenvopwe@gmail.com>
2020-05-13 23:42:28 +0200

    linting enhanced for stylelint, settings updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
+    extends: ['react-app', 'airbnb', 'prettier', 'prettier/react'],
+    plugins: ['react', 'prettier'],
     env: {
         browser: true,
         es6: true,
+        jest: true,
+        node: true,
     },
-    parser: 'babel-eslint',
-    extends: ['airbnb', 'prettier', 'prettier/react'],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
     },
+    parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -17,16 +20,10 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: ['react', 'prettier'],
     rules: {
         'prettier/prettier': 'error',
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
         'react/forbid-prop-types': [0, { forbid: ['any'] }],
         'react/prop-types': 0,
-    },
-    env: {
-        jest: true,
-        browser: true,
-        node: true,
     },
 }
