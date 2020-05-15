17700d355a0eef72f8a4db50730a9f2652421346
Andreas Olck <andreasolck@Andreass-MBP.fritz.box>
2020-05-15 17:24:05 +0200

    Slider initialized

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.scss'
+import LandingPage from './components/pages/LandingPage/LandingPage.page'
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-            </header>
-        </div>
-    )
+    return <LandingPage />
 }
 
 export default App
---
+++ src/components/atoms/RightSlideButton/RightSlideButton.atom.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function RightSlideButton() {
+    return <div className="slide-right">&gt;</div>
+}
+
+export default RightSlideButton
