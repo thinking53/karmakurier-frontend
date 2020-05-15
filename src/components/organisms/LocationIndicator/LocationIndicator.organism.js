56faff5a8757bf548ea979d8de76e80ad440f148
Sven Vowe <svenvopwe@gmail.com>
2020-05-16 00:00:01 +0200

    basic grid system

---
+++ src/components/organisms/LocationIndicator/LocationIndicator.organism.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './LocationIndicator.organism.scss'
+
+function LocationIndicator() {
+    return <div className="location-indicator">I</div>
+}
+
+export default LocationIndicator
--- src/components/pages/LandingPage/LandingPage.page.js
+++ src/components/pages/LandingPage/LandingPage.page.js
@@ -1,8 +1,31 @@
 import React from 'react'
-import Slider from '../../templates/Slider/Slider.template'
+import Header from '../../organisms/Header/Header.organism'
+import Footer from '../../organisms/Footer/Footer.organism'
+import './LandingPage.page.scss'
+import LocationIndicator from '../../organisms/LocationIndicator/LocationIndicator.organism'
 
 function LandingPage() {
-    return <Slider />
+    return (
+        <div className="landing-page">
+            <Header />
+            <div className="content">
+                <div className="section">
+                    <h1>Section </h1>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="section">
+                    <h1>Section </h1>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="section">
+                    <h1>Section </h1>
+                    <p>Lorem Ipsum</p>
+                </div>
+            </div>
+            <LocationIndicator />
+            <Footer />
+        </div>
+    )
 }
 
 export default LandingPage
