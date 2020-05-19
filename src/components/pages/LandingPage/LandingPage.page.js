ed41c6010abd6dfa1326d336a90d3a336bcf7fd3
Sven Vowe <svenvopwe@gmail.com>
2020-05-19 17:43:51 +0200

    init state

--- src/components/pages/LandingPage/LandingPage.page.js
+++ src/components/pages/LandingPage/LandingPage.page.js
@@ -1,20 +1,42 @@
 import React from 'react'
-import Header from '../../organisms/Header/Header.organism'
+import { useInView } from 'react-intersection-observer'
 import Footer from '../../organisms/Footer/Footer.organism'
-import './LandingPage.page.scss'
+import Header from '../../organisms/Header/Header.organism'
 import LocationIndicator from '../../organisms/LocationIndicator/LocationIndicator.organism'
+import Chores from '../../templates/Chores/Chores.template'
+import SliderHeadline from '../../templates/Slider/Slider.template'
+import './LandingPage.page.scss'
 
-function LandingPage() {
+const LandingPage = () => {
+    // see: https://github.com/researchgate/react-intersection-observer/blob/master/README.md
+    // see: https://github.com/thebuilder/react-intersection-observer/blob/HEAD/docs/Recipes.md#lazy-image-load
+    const [ref /* inView, entry */] = useInView({
+        /* Optional options */
+        threshold: 0,
+    })
+
     return (
         <div className="landing-page">
             <Header />
             <div className="content">
-                {[1, 2, 3, 4, 5].map(() => (
-                    <div className="section">
-                        <h1>karmakurier hilft</h1>
-                        <p>Coming soon...</p>
-                    </div>
-                ))}
+                <div ref={ref} className="section">
+                    <h1>karmakurier hilft bei Besorgungen</h1>
+                    <Chores />
+                </div>
+                <div className="section">
+                    <SliderHeadline headline="Wie funktioniert karmakurier?" />
+                </div>
+                <div className="section">
+                    <h1>Unser Karmaprogramm</h1>
+                </div>
+                <div className="section">
+                    <h1>Unsere Unterstützer</h1>
+                </div>
+                <div className="section">
+                    <h1>
+                        Werden Sie Teil von <span className="highlight">karmakurier</span>
+                    </h1>
+                </div>
             </div>
             <LocationIndicator />
             <Footer />
