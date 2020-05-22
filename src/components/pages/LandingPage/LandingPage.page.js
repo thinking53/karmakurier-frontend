1aa85330e501113eaed8de2436acc86744b62180
Sven Vowe <svenvopwe@gmail.com>
2020-05-22 11:48:03 +0200

    working loading indicator

--- src/components/pages/LandingPage/LandingPage.page.js
+++ src/components/pages/LandingPage/LandingPage.page.js
@@ -1,44 +1,52 @@
-import React from 'react'
-import { useInView } from 'react-intersection-observer'
+import React, { useState } from 'react'
+import { InView } from 'react-intersection-observer'
 import Footer from '../../organisms/Footer/Footer.organism'
 import Header from '../../organisms/Header/Header.organism'
 import LocationIndicator from '../../organisms/LocationIndicator/LocationIndicator.organism'
 import Chores from '../../templates/Chores/Chores.template'
-import SliderHeadline from '../../templates/Slider/Slider.template'
 import './LandingPage.page.scss'
 
 const LandingPage = () => {
+    const [activeSection, setActiveSection] = useState(0)
+    const threshold = 0.8
+    const handleViewChange = (visible, sectionId) => {
+        if (visible) {
+            setActiveSection(sectionId)
+        }
+    }
+
     // see: https://github.com/researchgate/react-intersection-observer/blob/master/README.md
     // see: https://github.com/thebuilder/react-intersection-observer/blob/HEAD/docs/Recipes.md#lazy-image-load
-    const [ref /* inView, entry */] = useInView({
-        /* Optional options */
-        threshold: 0,
-    })
 
     return (
         <div className="landing-page">
             <Header />
             <div className="content">
-                <div ref={ref} className="section">
-                    <h1>karmakurier hilft bei Besorgungen</h1>
-                    <Chores />
-                </div>
-                <div className="section">
-                    <SliderHeadline headline="Wie funktioniert karmakurier?" />
-                </div>
-                <div className="section">
-                    <h1>Unser Karmaprogramm</h1>
-                </div>
-                <div className="section">
-                    <h1>Unsere Unterstützer</h1>
-                </div>
-                <div className="section">
-                    <h1>
-                        Werden Sie Teil von <span className="highlight">karmakurier</span>
-                    </h1>
-                </div>
+                <InView threshold={threshold} onChange={(inView) => handleViewChange(inView, 0)}>
+                    <div className="section">
+                        <h1>Karmakurier hilft bei Besorgungen</h1>
+                        <Chores />
+                    </div>
+                </InView>
+                <InView threshold={threshold} onChange={(inView) => handleViewChange(inView, 1)}>
+                    <div className="section">
+                        <h1>Unser Karmaprogramm</h1>
+                    </div>
+                </InView>
+                <InView threshold={threshold} onChange={(inView) => handleViewChange(inView, 2)}>
+                    <div className="section">
+                        <h1>Unsere Unterstützer</h1>
+                    </div>
+                </InView>
+                <InView threshold={threshold} onChange={(inView) => handleViewChange(inView, 3)}>
+                    <div className="section">
+                        <h1>
+                            Werden Sie Teil von <span className="highlight">karmakurier</span>
+                        </h1>
+                    </div>
+                </InView>
             </div>
-            <LocationIndicator />
+            <LocationIndicator sectionCount="4" activeSection={activeSection} />
             <Footer />
         </div>
     )
